92c875e8c973d5874aa231a9c8aeb4511114ce7a
Jon Mease <jon.mease@gmail.com>
2020-06-21 05:56:53 -0400

    fix wheel by cleaning output directory

--- repos/kaleido/py/setup.py
+++ repos/kaleido/py/setup.py
@@ -2,6 +2,8 @@ import setuptools
 import os
 import shutil
 from setuptools import setup, Command
+import glob
+import distutils.util
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,6 +13,31 @@ def package_files(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
 
 
 class CopyExecutable(Command):
@@ -30,12 +57,33 @@ class CopyExecutable(Command):
             os.path.join(here, 'kaleido', 'executable')
         )
 
+
+class PackageWheel(Command):
+    description = "Build Wheel Package"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.run_command("clean")
+        self.run_command("copy_executable")
+        cmd_obj = self.distribution.get_command_obj('bdist_wheel')
+
+        # Use current platform as plat_name, but replace linux with manylinux2014
+        cmd_obj.plat_name = distutils.util.get_platform().replace("linux-", "manylinux2014-")
+        cmd_obj.python_tag = 'py2.py3'
+        self.run_command("bdist_wheel")
+
 setup(
     name="kaleido",
-    version="0.0.1a2",
+    version="0.0.1a3",
     packages=["kaleido", "kaleido.scopes"],
     package_data={'kaleido': package_files("kaleido/executable")},
     cmdclass=dict(
-        copy_executable=CopyExecutable,
+        copy_executable=CopyExecutable, clean=CleanCommand, package=PackageWheel
     )
 )
