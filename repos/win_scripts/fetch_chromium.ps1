d960dc2cf4d4d1d2f380642f6ab38bfded6fcd48
Andrew Pikul <ajpikul@gmail.com>
2024-04-19 17:00:38 -0500

    Recombine script files

--- repos/win_scripts/fetch_chromium.ps1
+++ repos/win_scripts/fetch_chromium.ps1
@@ -3,7 +3,7 @@
 # But windows now seems to depend on something else that gets pulled in during the update
 
 # Commits
-$env:DEPOT_TOOLS_COMMIT=""
+$env:DEPOT_TOOLS_COMMIT="" # See Note: above
 $env:CHROMIUM_VERSION_TAG="124.0.6367.60"
 
 # Tell gclient to use local Vistual Studio install
@@ -19,9 +19,12 @@ cd depot_tools
 # Add cloned depot_tools directory to PATH
 $env:path = "$pwd;$pwd\bootstrap;$env:path"
 echo $env:path
+
+# GCLIENT_PY3 was experimental when this was configured
 $env:GCLIENT_PY3=0
 
-$env:DEPOT_TOOLS_UPDATE=1
+# See Note: above
+#$env:DEPOT_TOOLS_UPDATE=1
 
 # Reset to proper commit
 # Note: You can clone individual branches/tags but not commits
@@ -29,6 +32,20 @@ git reset --hard ${Env:DEPOT_TOOLS_COMMIT}
 git clean -ffd
 git --no-pager log -2
 
+cd ..
+
+cmd.exe /c gclient sync -D --force --reset --no-history --jobs=3 --revision=%CHROMIUM_VERSION_TAG% # google wants gclient run from cmd, no ps
+
+cd src
+# Append kaleido section to headless build file (src\headless\BUILD.gn)
+cat ..\win_scripts\build_target.py | Add-Content -Path .\headless\BUILD.gn
+
+## Write out credits
+python3 ..\src\tools\licenses\licenses.py credits *> ..\CREDITS.html
+
+## Go back to root directory
 cd ..\..
+
+
 
 
--- repos/win_scripts/sync_chromium.bat
+++
@@ -1,5 +0,0 @@
-# Change to cloned src/ directory
-cd repos\src
-gclient sync -D --force --reset --no-history --jobs=3 --revision=%CHROMIUM_VERSION_TAG%
-gclient runhooks
-cd ..\..
