a41cbc546ec80638321c9f504a9020211218814d
Andrew Pikul <ajpikul@gmail.com>
2024-04-20 11:26:37 -0500

    Attempt a init w/o update of depot_tools

--- repos/win_scripts/fetch_chromium.ps1
+++ repos/win_scripts/fetch_chromium.ps1
@@ -1,12 +1,9 @@
-# Note: we'd like to set a depot tools commit close to the chromium version
-# And we'd like to set DEPOT_TOOLS_UPDATE to 0 so it doesn't update
-# But windows now seems to depend on something else that gets pulled in during the update
-
 # Commits
-$env:DEPOT_TOOLS_COMMIT="" # See Note: above
+$env:DEPOT_TOOLS_COMMIT="f9f61a9d7c0c76a71dc1db860d1994c53c8aa148"
 $env:CHROMIUM_VERSION_TAG="108.0.5359.125"
-# requires windows 10 sdk version 10.0.20348.0 which durng vscode install is present in visual studio community: 2023 individual components
-# not sure if it woudl have come with older depot_tools
+# requires windows 10 sdk version 10.0.20348.0 which,\
+# durng vs community install, can be included by going to
+# "visual studio community: 2023" --> "individual components"
 
 # Tell gclient to use local Vistual Studio install
 $env:DEPOT_TOOLS_WIN_TOOLCHAIN=0
@@ -18,25 +15,36 @@ cd repos
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cd depot_tools
 
-# Add cloned depot_tools directory to PATH
-$env:path = "$pwd;$pwd\bootstrap;$env:path"
-echo $env:path
+
+git reset --hard ${env:DEPOT_TOOLS_COMMIT}
+git clean -ffd
 
 # GCLIENT_PY3 was experimental when this was configured
 $env:GCLIENT_PY3=0
 
-# See Note: above
-#$env:DEPOT_TOOLS_UPDATE=1
+# Add cloned depot_tools directory to PATH
+$env:path = "$pwd;$pwd\bootstrap;$env:path"
+echo $env:path
 
 # Reset to proper commit
-# Note: You can clone individual branches/tags but not commits
-git reset --hard ${Env:DEPOT_TOOLS_COMMIT}
-git clean -ffd
 git --no-pager log -2
+
+
+# TODO: READ IF UPDATING (not actually a TODO, just a highlight to attract your attention)
+# This will turn off auto-update so we can stick with the version we picked
+$env:DEPOT_TOOLS_UPDATE=0
+# However, the .\update_dpot_tools.bat file which we don't want to run also has two calls at the end
+# which we do need to run! Google design flaw.
+cmd.exe /c call "%~dp0\cipd_bin_setup.bad"
+cmd.exe /c call "%~dp0\bootstrap\win_tools.bad"
+# If you're updating the DEPOT_TOOLS_COMMIT, you should read the skipped bat file to make sure you're doing
+# the init steps like above and whatever else, but not updating git!
 
 cd ..
 
-cmd.exe /c gclient sync -D --force --reset --no-history --jobs=3 --revision=%CHROMIUM_VERSION_TAG% # google wants gclient run from cmd, no ps
+cmd.exe /c gclient sync -D --force --reset --no-history --jobs=3 --revision=%CHROMIUM_VERSION_TAG% 
+# google wants cmd.exe not powershell
+# cmd not strictly necessary as gclient is a .bat that invokes cmd intrinsically but better safe than sorry
 
 cd src
 # Append kaleido section to headless build file (src\headless\BUILD.gn)
