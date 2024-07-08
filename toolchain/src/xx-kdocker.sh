a41d281531a592582b74239b0d297875dcef308e
Andrew Pikul <ajpikul@gmail.com>
2024-07-08 10:57:36 -0400

    Rework docker build a bit:
    To make it make sense, we had to let the user log in as their local
    user.
    
    We also didn't need all that git clone stuff, they can do that
    themselves with a command, we now mount the main repo to /usr/share, and
    we give them a convenience option to reclone that to their home
    directory.

--- toolchain/src/xx-kdocker.sh
+++ toolchain/src/xx-kdocker.sh
@@ -8,8 +8,8 @@ IMAGE="cimg/python:3.12.3"
 usage=(
   "kdocker is convenience script to start dockers for building on linux."
   ""
-  "You need to be in the docker group to run it. Script pulls an image and ads"
-  "the user who ran the script the docker, and mount the project directory as well."
+  "You need to be in the docker group to run kdocker. kdocker 1) pulls an image 2) adds"
+  "the user who ran kdocker, 3) mounts the project directory to /usr/share/kaleido."
   "See bottom for tips on detaching and reattaching to the docker."
   ""
   "Usage (DO NOT USE --long-flags=something, just --long-flag something):"
@@ -18,7 +18,10 @@ usage=(
   "Display this help:"
   "kdocker [-h|--h]"
   ""
-  "kdocker [-i|--image IMAGE] [-d|--detach] [[-g|--git]|[-r|--repo REPO]] COMMAND"
+  "kdocker [-u USER] [-i|--image IMAGE] [-d|--detach] COMMAND"
+  ""
+  "-u|--user USER     If you are running kdocker as sudo, you can use this to specify which"
+  "                   user you normally are."
   ""
   "-i|--image IMAGE   IMAGE will be used instead of the default, $IMAGE."
   "                   If this is not the same as circle-ci, please submit a pull request"
@@ -27,57 +30,68 @@ usage=(
   "-d|--detach        This will run the container and immediately detach."
   "                   The container will also exit immediately after finishing."
   ""
-  "-g|--git           The default behavior is to mount your plotly/kaleido git folder"
-  "                   into the docker, but this will try to re-clone it instead. Your"
-  "                   current working directory will be mounted into project/."
-  ""
-  "-r|--repo REPO     This is like --git, except it will run \`git clone REPO\`. You can pass"
-  "                   flags and stuff in quotes: \`-r \"-b my_branch my_user/my_repo\"\`."
-  "                   It will override -g."
-  ""
+  "-c|--copy          This will reclone your git project to ~/kaleido and also patch over"
+  "                   all uncommited, staged and unstaged, tracked changes. Untracked changes"
+  "                   will not be cloned over. "
+  "                   Hint: Use \`git add -N PATH\` to track files without staging them for commit."
+  "                   Hint: Don't work out of the clone ~/kaleido directory."
+  "                   Hint: Use \`refresh\` to re-clone/patch ~/kaleido after changes."
+  "                   Hint: If you use -c (or `refresh`), kaleido build commands (set_version, etc)"
+  "                         will always be run from ~/kaleido, not /usr/share/kaleido."
   "Docker tips:"
   "      Ending the first session will always end the docker. \`ctl+d\` will exit bash and session."
   "     \`ctl+p ctl+q\` (instead of \`ctl+d\`) will leave bash running. You can reattach to (only)"
   "      the first session with: \`docker attach CONTAINER_NAME\`. Containers can be listed with"
   "     \`docker container ls\`. You can get a new secondary session with:"
-  "     \`docker exec -it CONTAINER_NAME bash\`."
+  "     \`docker exec --user \$USER -it CONTAINER_NAME bash\`."
 )
 
 ## PROCESS FLAGS
-COMMAND="sudo apt-get update; sudo useradd --uid=$UID --shell /bin/bash --create-home $USER; echo '$USER ALL=NOPASSWD: ALL' | sudo tee -a /etc/sudoers.d/50-circleci &> /dev/null;"
-USER_COMMAND='export PATH+=:~/kaleido/bin/; '
 DETACH=""
-CLONE=false
 NO_VERBOSE=true
-REPO="plotly/kaleido"
+COPY=false
+LOCAL_USER="$USER"
 while (( $# )); do
   case $1 in
     -h|--help)        printf "%s\n" "${usage[@]}"; exit 0  ;;
     -i|--image)       shift; IMAGE="$1"                    ;;
+    -u|--user)        shift; LOCAL_USER="$1"               ;;
     -d|--detach)      DETACH="d"                           ;;
-    -g|--git)         CLONE=true                           ;;
-    -r|--repo)        shift; CLONE=true; REPO="$1"         ;;
     -v|--verbose)     NO_VERBOSE=false                     ;;
-    *)                break;;
+    -c|--copy)        COPY=true                            ;;
+    *)                break                                ;;
   esac
   shift
 done
+LOCAL_UID="$(id -u $LOCAL_USER)"
+COMMAND="true || sudo apt-get update; sudo useradd --uid=$LOCAL_UID --shell /bin/bash --create-home $LOCAL_USER; echo '$LOCAL_USER ALL=NOPASSWD: ALL' | sudo tee -a /etc/sudoers.d/50-circleci &> /dev/null;"
+USER_COMMAND=''
 
 $NO_VERBOSE || echo "Running xx-kdocker.sh"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- $(readlink -f -- "${BASH_SOURCE[0]}") )" &> /dev/null && pwd )
 . "$SCRIPT_DIR/include/utilities.sh"
 
-VOLUME="$MAIN_DIR:/home/$USER/kaleido"
-if $CLONE; then
-  USER_COMMAND+="git clone $REPO; "
-  VOLUME="$(pwd):/home/$USER/"
-fi
+VOLUME="$MAIN_DIR:/usr/share/kaleido"
 if [[ -n "${@}" ]]; then
   USER_COMMAND+="${@}; "
 fi
-COMMAND+="echo '$USER_COMMAND' | sudo tee -a /home/$USER/.temp_script.sh &> /dev/null; echo . /home/$USER/.temp_script.sh | sudo tee -a /home/$USER/.bash_login &> /dev/null; "
-COMMAND+="sudo su - $USER"
+
+SUDO="sudo sudo -u $LOCAL_USER"
+_OUT="1> /dev/null"
+BASH_LOGIN="/home/$LOCAL_USER/.bash_login"
+TEMP_SCRIPT="/home/$LOCAL_USER/.temp_script.sh"
+COMMAND+="\
+  echo '$USER_COMMAND' | $SUDO tee -a $TEMP_SCRIPT $_OUT; \
+  echo . $TEMP_SCRIPT | $SUDO tee -a $BASH_LOGIN $_OUT; \
+  echo 'rm -f $TEMP_SCRIPT' | $SUDO tee -a $BASH_LOGIN $_OUT; \
+  echo 'head -n -3 $BASH_LOGIN > $BASH_LOGIN' | $SUDO tee -a $BASH_LOGIN $_OUT; "
+if $COPY; then
+  COMMAND+="$SUDO -- git clone /usr/share/kaleido /home/$LOCAL_USER/kaleido; \
+    $SUDO git -C /usr/share/kaleido diff -p HEAD | $SUDO tee /home/$LOCAL_USER/.git_patch_1 $_OUT; \
+    $SUDO git -C /home/$LOCAL_USER/kaleido apply /home/$LOCAL_USER/.git_patch_1; "
+fi
+COMMAND+="sudo su - $LOCAL_USER"
 
 docker pull $IMAGE
-docker container run --rm -it$DETACH -v "$VOLUME" "$IMAGE" bash -c "$COMMAND"
+docker container run -e TERM=$TERM --rm -it$DETACH -v "$VOLUME" "$IMAGE" bash -c "$COMMAND"
