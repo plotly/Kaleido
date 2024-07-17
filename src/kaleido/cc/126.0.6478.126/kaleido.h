2e2f588a4cacec90eecce3ae71320a9760d3b753
Andrew Pikul <ajpikul@gmail.com>
2024-07-17 00:02:49 -0400

    Create kaleido base for sanity testing structure

---
+++ src/kaleido/cc/126.0.6478.126/kaleido.h
@@ -0,0 +1,17 @@
+/*
+#ifndef KALEIDO_H_
+#define KALEIDO_H_
+
+#include "content/public/app/content_main.h"
+
+namespace kaleido {
+
+// Start the headless shell applications.
+// Note that the |ContentMainDelegate| is ignored and
+// |HeadlessContentMainDelegate| is used instead.
+int HeadlessShellMain(const content::ContentMainParams params);
+
+}
+
+#endif  // KALEIDO_H_
+*/
