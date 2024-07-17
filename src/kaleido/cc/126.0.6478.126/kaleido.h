ad80c1c4b48ea8aa28e72c451d60dd676ea84395
Andrew Pikul <ajpikul@gmail.com>
2024-07-17 18:07:11 +0000

    Revert "Move boiler plate into app.cc"
    This reverts commit 0cef37d5b3a0e0d47570926bcf5625330a0c403a.

--- src/kaleido/cc/126.0.6478.126/kaleido.h
+++ src/kaleido/cc/126.0.6478.126/kaleido.h
@@ -1,26 +1,8 @@
 #ifndef KALEIDO_H_
 #define KALEIDO_H_
 
-// Fundamental chromium includes
-#include "content/public/app/content_main.h"
-#include "headless/public/headless_browser.h"
 
 namespace kaleido {
-  class Kaleido {
-	  public:
-      Kaleido(content::ContentMainParams);
-	    Kaleido(const Kaleido&) = delete;
-	    Kaleido& operator=(const Kaleido&) = delete;
-	    ~Kaleido() = default;
-
-      void OnBrowserStart(headless::HeadlessBrowser* browser);
-    private:
-      void ShutdownSoon();
-      void Shutdown();
-      raw_ptr<headless::HeadlessBrowser> browser_ = nullptr;
-  };
-  void ChildProcess(content::ContentMainParams);
+  class Kaleido;
 }
-
-
 #endif  // KALEIDO_H_
