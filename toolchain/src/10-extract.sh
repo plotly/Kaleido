62e762f1bfc975c519f2076d18f708ffe5574549
Andrew Pikul <ajpikul@gmail.com>
2024-07-31 13:47:28 -0400

    Start removing readlink x 9

--- toolchain/src/10-extract.sh
+++ toolchain/src/10-extract.sh
@@ -124,6 +124,7 @@ missing=False, annotate=False, relative=True)")")
       $NO_VERBOSE || echo "SRC_DIR:         ${SRC_DIR}"
       $NO_VERBOSE || echo "BUILD_DIR:       ${BUILD_DIR}"
       MKDIR_PATH="$(dirname "${BUILD_DIR}")${f}"
+      $NO_VERBOSE || echo "First mkdir path: ${MKDIR_PATH}"
       if [ ! -d "${MKDIR_PATH}" ]; then
         MKDIR_PATH="$(dirname "${MKDIR_PATH}")"
       fi
