996c4fdd80dcc0e781b76b4fbcaae4ea499cb90b
Neyberson <neyberatencio@gmail.com>
2024-10-21 16:48:30 -0500

    Improve app.py to can test it

--- app.py
+++ app.py
@@ -11,19 +11,24 @@ from choreographer import Browser
 script_path = Path(__file__).resolve().parent / "index.html"
 
 # pdf and eps temporarily disabled
-_all_formats_ = ("png", "jpg", "jpeg", "webp", "svg", "json") # pdf and eps
+
 _text_formats_ = ("svg", "json",) # eps
 
 _scope_flags_ = ("plotlyjs", "mathjax", "topojson", "mapbox_access_token")
 
-def make_spec(self, figure, format=None, width=None, height=None, scale=None):
+def make_spec(figure, format=None, width=None, height=None, scale=None):
+    default_format = "png"
+    default_scale = 1
+    default_width = 700
+    default_height = 500
+    _all_formats = ("png", "jpg", "jpeg", "webp", "svg", "json") # pdf and eps
     # TODO: validate args
     if isinstance(figure, Figure):
         figure = figure.to_dict()
 
     # Apply default format and scale
-    format = format if format is not None else self.default_format
-    scale = scale if scale is not None else self.default_scale
+    format = format if format is not None else default_format
+    scale = scale if scale is not None else default_scale
 
     # Get figure layout
     layout = figure.get("layout", {})
@@ -33,13 +38,13 @@ def make_spec(self, figure, format=None, width=None, height=None, scale=None):
             width
             or layout.get("width", None)
             or layout.get("template", {}).get("layout", {}).get("width", None)
-            or self.default_width
+            or default_width
     )
     height = (
             height
             or layout.get("height", None)
             or layout.get("template", {}).get("layout", {}).get("height", None)
-            or self.default_height
+            or default_height
     )
 
     # Normalize format
@@ -48,8 +53,8 @@ def make_spec(self, figure, format=None, width=None, height=None, scale=None):
     if format == 'jpg':
         format = 'jpeg'
 
-    if format not in self._all_formats:
-        supported_formats_str = repr(list(self._all_formats))
+    if format not in _all_formats:
+        supported_formats_str = repr(list(_all_formats))
         raise ValueError(
             "Invalid format '{original_format}'.\n"
             "    Supported formats: {supported_formats_str}"
@@ -126,3 +131,52 @@ async def to_image(spec, topojson=None, mapbox_token=None):
         else:
             img = str.encode(img)
         return img
+
+
+def transform(figure, format=None, width=None, height=None, scale=None):
+    spec = make_spec(figure, format=format, width=width, height=height, scale=scale)
+    return asyncio.run(to_image(spec))
+
+
+############
+########TEST
+############
+import os
+
+dirIn = Path(__file__).resolve().parent / "mocks/"
+ALL_MOCKS = [os.path.splitext(a)[0] for a in os.listdir(dirIn) if a.endswith('.json')]
+ALL_MOCKS.sort()
+allNames = ALL_MOCKS
+
+
+for name in allNames:
+    with open(os.path.join(dirIn, name + '.json'), 'r') as _in :
+        try:
+            fig = json.load(_in)
+        except Exception as e:
+            print("No load")
+            print(e)
+            print(_in)
+            print("***")
+            continue
+
+        width = 700
+        height = 500
+        if 'layout' in fig :
+            layout = fig['layout']
+            if 'autosize' not in layout or layout['autosize'] != True :
+                if 'width' in layout :
+                    width = layout['width']
+                if 'height' in layout :
+                    height = layout['height']
+        try:
+            img_data = transform(fig, format="png", width=width, height=height)
+            output_file = f"./results/{name}.png"
+            with open(output_file, "wb") as out_file:
+                out_file.write(img_data)
+            print(f"Image saved to {output_file}")
+        except Exception as e:
+            print("No to image")
+            print(e)
+            print(_in)
+            print("***")
