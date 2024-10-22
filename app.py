385a523d9256f03cb3e69385d4a155f8d6dbfde6
Neyberson <neyberatencio@gmail.com>
2024-10-22 12:12:48 -0500

    Create process_images() and update to_image() use in app.py

--- app.py
+++ app.py
@@ -1,43 +1,46 @@
 import os
 import json
+import asyncio
 
 from pathlib import Path
-from .baile.process_images import transform
+from .baile import to_image
 
 dirIn = Path(__file__).resolve().parent / "mocks/"
 ALL_MOCKS = [os.path.splitext(a)[0] for a in os.listdir(dirIn) if a.endswith(".json")]
 ALL_MOCKS.sort()
 allNames = ALL_MOCKS
 
+async def process_images():
+    for name in allNames:
+        with open(os.path.join(dirIn, name + ".json"), "r") as _in:
+            try:
+                fig = json.load(_in)
+            except Exception as e:
+                print("No load")
+                print(e)
+                print(_in)
+                print("***")
+                continue
 
-for name in allNames:
-    with open(os.path.join(dirIn, name + ".json"), "r") as _in:
-        try:
-            fig = json.load(_in)
-        except Exception as e:
-            print("No load")
-            print(e)
-            print(_in)
-            print("***")
-            continue
+            width = 700
+            height = 500
+            if "layout" in fig:
+                layout = fig["layout"]
+                if "autosize" not in layout or layout["autosize"] != True:
+                    if "width" in layout:
+                        width = layout["width"]
+                    if "height" in layout:
+                        height = layout["height"]
+            try:
+                img_data = await to_image(fig, format="png", width=width, height=height)
+                output_file = f"./results/{name}.png"
+                with open(output_file, "wb") as out_file:
+                    out_file.write(img_data)
+                print(f"Image saved to {output_file}")
+            except Exception as e:
+                print("No to image")
+                print(e)
+                print(_in)
+                print("***")
 
-        width = 700
-        height = 500
-        if "layout" in fig:
-            layout = fig["layout"]
-            if "autosize" not in layout or layout["autosize"] != True:
-                if "width" in layout:
-                    width = layout["width"]
-                if "height" in layout:
-                    height = layout["height"]
-        try:
-            img_data = transform(fig, format="png", width=width, height=height)
-            output_file = f"./results/{name}.png"
-            with open(output_file, "wb") as out_file:
-                out_file.write(img_data)
-            print(f"Image saved to {output_file}")
-        except Exception as e:
-            print("No to image")
-            print(e)
-            print(_in)
-            print("***")
+asyncio.run(process_images())
