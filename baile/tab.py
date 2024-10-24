01e079e5088574c6d5c8acb61dbbc780012f92b4
Neyberson <neyberatencio@gmail.com>
2024-10-24 16:31:06 -0500

    Delete useless method in Tab

--- baile/tab.py
+++ baile/tab.py
@@ -1,15 +1,9 @@
 import choreographer as choreo
-from .prepare import SCRIPT_PATH
 
 
 class Tab(choreo.tab.Tab):
     def __init__(self, target_id, browser):
         super().__init__(target_id, browser)
-
-    @classmethod
-    async def create(cls, browser):
-        tab = await browser.create_tab(SCRIPT_PATH.as_uri())
-        return cls(tab.target_id, browser)
 
     async def reload(self):
         return await self.send_command(
