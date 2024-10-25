4e7da61968283a3c28bff92fac59cf6f27b71ff5
Neyberson <neyberatencio@gmail.com>
2024-10-25 18:31:00 -0500

    Update app.py

--- tests/app.py
+++ tests/app.py
@@ -8,24 +8,17 @@ import baile
 # Extract jsons of mocks
 dir_in = Path(__file__).resolve().parent / "mocks"
 results_dir = Path(__file__).resolve().parent / "images"
-ALL_MOCKS = [os.path.splitext(a)[0] for a in os.listdir(dir_in) if a.endswith(".json")]
-ALL_MOCKS.sort()
-all_names = ALL_MOCKS
 
 
 # Loop to generate images of the jsons
 async def process_images():
-    for name in all_names:
-        # read json
-        json_path = os.path.join(dir_in, name + ".json")
-        try:
-            # Process to generete images of the json
-            await baile.to_image(json_path, path=str(results_dir))
-        except Exception as e:
-            print("No to image")
-            print(e)
-            print(json_path)
-            print("***")
+    try:
+        # Process to generete images of the json
+        await baile.to_image(path_figs=dir_in, path=str(results_dir))
+    except Exception as e:
+        print("No to image")
+        print(e)
+        print("***")
 
 
 # Run the loop
