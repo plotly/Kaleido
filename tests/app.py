06aef17b9ba185342e2c5448f2a0acb3cfc19da9
Neyberson <neyberatencio@gmail.com>
2024-10-31 16:00:02 -0500

    Improve app.py to use --benchmark flag

--- tests/app.py
+++ tests/app.py
@@ -1,7 +1,8 @@
-import asyncio
-import argparse
+from time import process_time
 from pathlib import Path
-
+import json
+import argparse
+import asyncio
 import baile
 
 # Extract jsons of mocks
@@ -9,16 +10,24 @@ dir_in = Path(__file__).resolve().parent / "mocks"
 results_dir = Path(__file__).resolve().parent / "images"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_tabs", type=int, default=4, help="Number of tabs")
-parser.add_argument("--path_mock", type=str, default=dir_in, help="Directory of mock file/s")
+parser.add_argument("--n_tabs", type=int, help="Number of tabs")
+parser.add_argument(
+    "--path_mock", type=str, default=dir_in, help="Directory of mock file/s"
+)
+parser.add_argument("--benchmark", action="store_true", help="Enable benchmarking")
 args = parser.parse_args()
 arg_dict = vars(args)
 
+# Improve the defaults
+if arg_dict["benchmark"] and not arg_dict["n_tabs"]:
+    arg_dict["n_tabs"] = 1
+elif not arg_dict["benchmark"] and not arg_dict["n_tabs"]:
+    arg_dict["n_tabs"] = 4
 
-# Loop to generate images of the jsons
+
+# Function to process the images
 async def process_images():
     try:
-        # Process to generete images of the json
         await baile.to_image(
             path_figs=arg_dict["path_mock"],
             path=str(results_dir),
@@ -26,11 +35,38 @@ async def process_images():
             debug=True,
             headless=False,
         )
+        return "Successful"
     except Exception as e:
         print("No to image".center(30, "%"))
         print(e)
         print("***")
+        return e
 
 
 # Run the loop
-asyncio.run(process_images())
+if __name__ == "__main__":
+    if arg_dict["benchmark"]:
+        # Set the dictionary
+        results = {
+            "execution_time": None,
+            "unit": "seconds",
+            "file": arg_dict["path_mock"],
+        }
+
+        t1_start = process_time()  # Start timing
+
+        # Measure the execution of process_images
+        result_message = asyncio.run(process_images())
+
+        t2_stop = process_time()  # Stop timing
+        results["execution_time"] = t2_stop - t1_start
+        if result_message == "Successful":
+            results["result"] = result_message
+        else:
+            results["error"] = result_message
+
+        # Convert results to JSON and print
+        print("Benchmark".center(10, "*"))
+        print(json.dumps(results, indent=4))
+    else:
+        asyncio.run(process_images())
