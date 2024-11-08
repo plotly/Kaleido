a4e3a6b524b03c902323f147de65811bd164db49
Neyberson <neyberatencio@gmail.com>
2024-11-08 12:16:36 -0500

    Create _get_name() function in custom_logging.py

--- baile/custom_logging.py
+++ baile/custom_logging.py
@@ -14,27 +14,33 @@ logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(stream=sys.stderr)
 logger.addHandler(handler)
 
+def _get_name():
+    upper_frame = inspect.currentframe().f_back.f_back
+    module = inspect.getmodule(upper_frame).__name__
+    module_function = upper_frame.f_code.co_name
+    return f"{module}:{module_function}"
+
 # Overwrite functions
 def debug2(message):
     function = inspect.stack()[0].function
-    logger._log(DEBUG2, f"{function.upper()}: {message}", ()) # The () is for the empty args
+    logger._log(DEBUG2, f"{function.upper()}:{_get_name()}: {message}", ()) # The () is for the empty args
 
 def debug1(message):
     function = inspect.stack()[0].function
-    logger.debug(f"{function.upper()}: {message}")
+    logger.debug(f"{function.upper()}:{_get_name()}: {message}")
 
 def info(message):
     function = inspect.stack()[0].function
-    logger.info(f"{function.upper()}: {message}")
+    logger.info(f"{function.upper()}:{_get_name()}: {message}")
 
 def error(message):
     function = inspect.stack()[0].function
-    logger.error(f"{function.upper()}: {message}")
+    logger.error(f"{function.upper()}:{_get_name()}: {message}")
 
 def warning(message):
     function = inspect.stack()[0].function
-    logger.warning(f"{function.upper()}: {message}")
+    logger.warning(f"{function.upper()}:{_get_name()}: {message}")
 
 def critical(message):
     function = inspect.stack()[0].function
-    logger.critical(f"{function.upper()}: {message}")
+    logger.critical(f"{function.upper()}:{_get_name()}: {message}")
