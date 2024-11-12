3da98d637621f34cf5596c0c06c31a9957f5dda8
Neyberson <neyberatencio@gmail.com>
2024-11-12 09:10:05 -0500

    Add and improve comments in custom_logging.py

--- baile/custom_logging.py
+++ baile/custom_logging.py
@@ -2,7 +2,7 @@ import sys
 import logging
 import inspect
 
-# new constants
+# new constant
 DEBUG2 = 5
 
 # Create the logging
@@ -11,6 +11,8 @@ logging.addLevelName(DEBUG2, "DEBUG2")
 
 # Set logger
 logger = logging.getLogger(__name__)
+
+# Set handler
 handler = logging.StreamHandler(stream=sys.stderr)
 logger.addHandler(handler)
 
@@ -28,7 +30,7 @@ def _get_name():
     return f"{module}:{module_function}()"
 
 
-# Overwrite functions
+# Custom debug with custom level
 def debug2(message):
     function = inspect.stack()[0].function
     logger._log(
@@ -36,26 +38,31 @@ def debug2(message):
     )  # The () is for the empty args
 
 
+# Overwrite function
 def debug1(message):
     function = inspect.stack()[0].function
     logger.debug(f"{function.upper()}:{_get_name()}: {message}")
 
 
+# Overwrite function
 def info(message):
     function = inspect.stack()[0].function
     logger.info(f"{function.upper()}:{_get_name()}: {message}")
 
 
+# Overwrite function
 def error(message):
     function = inspect.stack()[0].function
     logger.error(f"{function.upper()}:{_get_name()}: {message}")
 
 
+# Overwrite function
 def warning(message):
     function = inspect.stack()[0].function
     logger.warning(f"{function.upper()}:{_get_name()}: {message}")
 
 
+# Overwrite function
 def critical(message):
     function = inspect.stack()[0].function
     logger.critical(f"{function.upper()}:{_get_name()}: {message}")
