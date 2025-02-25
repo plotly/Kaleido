373b44b8130e4b2de1a01444da340a4a167c2cd2
Andrew Pikul <ajpikul@gmail.com>
2025-02-25 18:09:48 -0500

    Test calc_fig[_sync]

---
+++ src/py/tests/test_calc_fig.py
@@ -0,0 +1,24 @@
+import warnings
+
+import logistro
+import pytest
+
+import kaleido
+
+# allows to create a browser pool for tests
+pytestmark = pytest.mark.asyncio(loop_scope="function")
+
+_logger = logistro.getLogger(__name__)
+
+
+async def test_calc_fig():
+    import plotly.express as px
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        fig = px.line(x=[1, 2, 3, 4], y=[1, 2, 3, 4])
+    img = await kaleido.calc_fig(fig)
+    assert img
+    img = kaleido.calc_fig_sync(fig)
+    assert img
+    assert True
