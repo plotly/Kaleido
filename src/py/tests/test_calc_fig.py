f367af2090d95f081c67c588f0c56775267102dd
Andrew Pikul <ajpikul@gmail.com>
2025-02-25 19:18:00 -0500

    Add tests for dictionary-style plotly.Figure

--- src/py/tests/test_calc_fig.py
+++ src/py/tests/test_calc_fig.py
@@ -17,10 +17,16 @@ async def test_calc_fig():
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         fig = px.line(x=[1, 2, 3, 4], y=[1, 2, 3, 4])
+
     img = await kaleido.calc_fig(fig)
     assert img
+
     img = kaleido.calc_fig_sync(fig)
     assert img
+
+    img = kaleido.calc_fig_sync(fig.to_dict())
+    assert img
+
     with pytest.raises(TypeError):
         # can't accept iterables
         img = kaleido.calc_fig_sync([fig, fig])
