dc514644538904f44b5d771aa7e13930433df18d
Liam Connors <connorsl@tcd.ie>
2025-03-24 15:27:01 -0400

    Update __init__.py

--- src/py/kaleido/__init__.py
+++ src/py/kaleido/__init__.py
@@ -1,5 +1,5 @@
 """
-Kaleido provides functions to convert plotly figures into various image formats.
+Kaleido is a library for generating static images from Plotly figures.
 
 Please see the README.md for more information and a quickstart.
 """
