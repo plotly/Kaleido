eb21c13199ed6f3da480ab84e0403f05496a50ad
Andrew Pikul <ajpikul@gmail.com>
2025-06-03 17:58:59 -0400

    Andrew/sanitize title (#340)
    * Escape user-supplied strings regex
    
    * Update CHANGELOG
    
    * Sanitize glob as well
    
    * Further sanitize title-filename
    
    * Improve changelog
    
    * More strictly enforce default fig name

--- src/py/kaleido/_fig_tools.py
+++ src/py/kaleido/_fig_tools.py
@@ -142,9 +142,10 @@ def build_fig_spec(fig, path, opts):  #  noqa: C901
             )
     if not full_path:
         _logger.debug("Looking for title")
-        prefix = (
-            fig.get("layout", {}).get("title", {}).get("text", "fig").replace(" ", "_")
-        )
+        prefix = fig.get("layout", {}).get("title", {}).get("text", "fig")
+        prefix = re.sub(r"[ \-]", "_", prefix)
+        prefix = re.sub(r"[^a-zA-Z0-9_]", "", prefix)
+        prefix = prefix or "fig"
         _logger.debug(f"Found: {prefix}")
         name = _next_filename(directory, prefix, ext)
         full_path = directory / name
