9dbeeeda521ef0eb3949dd2444dfa91f9319b8dc
Andrew Pikul <ajpikul@gmail.com>
2025-07-24 15:16:49 -0500

    Improve errors and typechecking in toolls.

--- src/py/kaleido/_fig_tools.py
+++ src/py/kaleido/_fig_tools.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+
 import glob
 import re
 from pathlib import Path
+from typing import TYPE_CHECKING, Any, Literal, TypedDict
 
 import logistro
+from typing_extensions import TypeGuard
+
+if TYPE_CHECKING:
+    """Just leaving it here in case."""
 
 _logger = logistro.getLogger(__name__)
 
@@ -11,10 +18,13 @@ DEFAULT_EXT = "png"
 DEFAULT_SCALE = 1
 DEFAULT_WIDTH = 700
 DEFAULT_HEIGHT = 500
-SUPPORTED_FORMATS = ("png", "jpg", "jpeg", "webp", "svg", "json", "pdf")  # pdf and eps
+SUPPORTED_FORMATS = ("png", "jpg", "jpeg", "webp", "svg", "json", "pdf")
+FormatString = Literal["png", "jpg", "jpeg", "webp", "svg", "json", "pdf"]
+
+Figurish = Any  # Be nice to make it more specific, dictionary or something
 
 
-def _is_figurish(o):
+def _is_figurish(o) -> TypeGuard[Figurish]:
     valid = hasattr(o, "to_dict") or (isinstance(o, dict) and "data" in o)
     if not valid:
         _logger.debug(
@@ -43,32 +53,49 @@ def _get_figure_dimensions(layout, width, height):
 
 
 def _get_format(extension):
-    # Normalize format
     original_format = extension
     extension = extension.lower()
     if extension == "jpg":
-        extension = "jpeg"
+        return "jpeg"
 
     if extension not in SUPPORTED_FORMATS:
-        supported_formats_str = repr(list(SUPPORTED_FORMATS))
         raise ValueError(
             f"Invalid format '{original_format}'.\n"
-            f"    Supported formats: {supported_formats_str}",
+            f"    Supported formats: {SUPPORTED_FORMATS!s}",
         )
     return extension
 
 
-def to_spec(figure, layout_opts):
+# Input of to_spec
+class LayoutOpts(TypedDict, total=False):
+    format: FormatString | None
+    scale: int | float
+    height: int | float
+    width: int | float
+
+
+# Output of to_spec
+class Spec(TypedDict):
+    format: FormatString
+    width: int | float
+    height: int | float
+    scale: int | float
+    data: Figurish
+
+
+def to_spec(figure, layout_opts: LayoutOpts) -> Spec:
     # Get figure layout
     layout = figure.get("layout", {})
 
     for k, v in layout_opts.items():
         if k == "format":
             if v is not None and not isinstance(v, (str)):
-                raise TypeError(f"{v} must be string or None")
+                raise TypeError(
+                    f"{k} must be one of {SUPPORTED_FORMATS!s} or None, not {v}.",
+                )
         elif k in ("scale", "height", "width"):
             if v is not None and not isinstance(v, (float, int)):
-                raise TypeError(f"{v} must be numeric or None")
+                raise TypeError(f"{k} must be numeric or None, not {v}.")
         else:
             raise AttributeError(f"Unknown key in layout options, {k}")
 
@@ -106,7 +133,7 @@ def _next_filename(path, prefix, ext):
     return f"{prefix}.{ext}" if n == 1 else f"{prefix}-{n}.{ext}"
 
 
-def build_fig_spec(fig, path, opts):  #  noqa: C901
+def build_fig_spec(fig, path, opts) -> tuple[Spec, Path]:  #  noqa: C901
     if not opts:
         opts = {}
 
@@ -127,18 +154,19 @@ def build_fig_spec(fig, path, opts):  #  noqa: C901
     spec = to_spec(fig, opts)
 
     ext = spec["format"]
-    full_path = None
+
+    full_path: Path
     if not path:
-        directory = Path()
+        directory = Path()  # use current Path
     elif path and (not path.suffix or path.is_dir()):
         if not path.is_dir():
-            raise ValueError(f"Directories will not be created for you: {path}")
+            raise ValueError(f"Please create all desired folders: {path}")
         directory = path
     else:
         full_path = path
         if not full_path.parent.is_dir():
             raise RuntimeError(
-                f"Cannot reach path {path}. Are all directories created?",
+                f"Cannot reach path {path.parent}. Are all directories created?",
             )
     if not full_path:
         _logger.debug("Looking for title")
