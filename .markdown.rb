6b2df2de12b862b0e1ad09f8a4261b2e4ba5ec85
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-26 21:26:07 +0200

    Update .markdown.rb
    less cryptic now

--- .markdown.rb
+++ .markdown.rb
@@ -1,3 +1,8 @@
+# MD026: Trailing punctuation in header - Disallows headers ending with specified punctuation characters
 rule 'MD026', :punctuation => '.,;:!'
+
+# MD013: Line length - Enforces a maximum line length, but ignores code blocks
 rule 'MD013', :ignore_code_blocks => true
+
+# MD033: Inline HTML - This rule is excluded, allowing inline HTML in Markdown files
 exclude_rule 'MD033'
