e5e0cd5273818c5ae8960ebc98889cd6c3d4eb53
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-14 16:27:28 -0500

    Test change cdn

--- src/py/scripts/update_cdn.py
+++ src/py/scripts/update_cdn.py
@@ -52,7 +52,7 @@ async def main():
     else:
         title = f"CDN not reachable for Plotly v{latest_version}"
         body = f"URL: {new_cdn} - invalid url"
-        out, _, reteval = await run(["gh", "issue", "list", "--search", title])
+        out, _, reteval = await run(["gh", "issue", "list", "--search", f"title:{title}"])
         print(title)
         print("reteval:", reteval)
         print("OUT", out)
