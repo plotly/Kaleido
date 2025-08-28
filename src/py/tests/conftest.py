501fb806fb7f8818a7b2facddff9c5f151cf3d42
Andrew Pikul <ajpikul@gmail.com>
2025-08-28 16:55:52 -0500

    Organize a bit conftest.py

--- src/py/tests/conftest.py
+++ src/py/tests/conftest.py
@@ -8,11 +8,20 @@ from hypothesis import HealthCheck, settings
 
 _logger = logistro.getLogger(__name__)
 
+# we turn off deadlines for macs and windows in CI
+# i'm under the impression that they are resource limited
+# and delays are often caused by sharing resources with other
+# virtualization neighbors.
+
 settings.register_profile(
     "ci",
     deadline=None,  # no per-example deadline
     suppress_health_check=(HealthCheck.too_slow,),  # avoid flaky "too slow" on CI
 )
+
+is_ci = os.getenv("GITHUB_ACTIONS") == "true" or os.getenv("CI") == "true"
+if is_ci and platform.system in {"Windows", "Darwin"}:
+    settings.load_profile("ci")
 
 
 # pytest shuts down its capture before logging/threads finish
@@ -28,8 +37,3 @@ def cleanup_logging_handlers(request):
                 handler.flush()
                 if isinstance(handler, logging.StreamHandler):
                     logging.root.removeHandler(handler)
-
-
-is_ci = os.getenv("GITHUB_ACTIONS") == "true" or os.getenv("CI") == "true"
-if is_ci and platform.system in {"Windows", "Darwin"}:
-    settings.load_profile("ci")
