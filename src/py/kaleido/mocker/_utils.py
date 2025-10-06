e63bd7a7ea63b83e63ce9a41621d77717a54bbd2
Andrew Pikul <ajpikul@gmail.com>
2025-10-06 16:41:38 -0500

    Add pickle-decompression logic + script to dates

--- src/py/kaleido/mocker/_utils.py
+++ src/py/kaleido/mocker/_utils.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
 import itertools
+import pickle
+import warnings
 from pathlib import Path
 from typing import TYPE_CHECKING, TypedDict
 
 import logistro
 import orjson
+
+try:
+    import zstandard as zstd
+
+    dctx = zstd.ZstdDecompressor()
+except ImportError:
+    dctx = None
 
 from ._args import args
 
@@ -41,27 +50,43 @@ def load_figures_from_paths(paths: list[Path]):
         if not path.is_file():
             raise RuntimeError(f"Path {path} is not a file.")
         _logger.info(f"Found file: {path!s}")
-        with path.open(encoding="utf-8") as file:
-            figure = orjson.loads(file.read())
-            for f, w, h, s in itertools.product(  # all combos
-                args.format,
-                args.width,
-                args.height,
-                args.scale,
-            ):
-                name = (
-                    f"{path.stem}.{f!s}"
-                    if not args.parameterize
-                    else f"{path.stem!s}-{w!s}x{h!s}@{s!s}.{f!s}"
+        if path.suffix == ".json":
+            with path.open(encoding="utf-8") as file:
+                figure = orjson.loads(file.read())
+        elif path.suffixes[-2:] == [".pkl", ".zst"]:
+            if not dctx:
+                raise RuntimeError(
+                    "Decompressing pickles requires zstandard optional dep.",
                 )
-                opts = {
-                    "scale": s,
-                    "width": w,
-                    "height": h,
-                }
-                _logger.info(f"Yielding spec: {name!s}")
-                yield {
-                    "fig": figure,
-                    "path": str(Path(args.output) / name),
-                    "opts": opts,
-                }
+            with path.open("rb") as file:
+                binary = dctx.decompress(file.read())
+                figure = pickle.loads(binary)  # noqa: S301 pickle warning
+        else:
+            warnings.warn(
+                f"Unrecognized path type, skipping: {path}",
+                category=UserWarning,
+                stacklevel=2,
+            )
+            continue
+        for f, w, h, s in itertools.product(  # all combos
+            args.format,
+            args.width,
+            args.height,
+            args.scale,
+        ):
+            name = (
+                f"{path.stem}.{f!s}"
+                if not args.parameterize
+                else f"{path.stem!s}-{w!s}x{h!s}@{s!s}.{f!s}"
+            )
+            opts = {
+                "scale": s,
+                "width": w,
+                "height": h,
+            }
+            _logger.info(f"Yielding spec: {name!s}")
+            yield {
+                "fig": figure,
+                "path": str(Path(args.output) / name),
+                "opts": opts,
+            }
